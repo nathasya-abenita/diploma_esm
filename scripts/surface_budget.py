0b48ca50cc52bdd720acb0b4dbdf7b845466b346
Nathasya Christien <nathasya.abenita@gmail.com>
2026-03-06 00:09:50 +0100

    reorganize surface budget script

--- scripts/surface_budget.py
+++ scripts/surface_budget.py
@@ -1,48 +1,90 @@
 import xarray as xr
 import matplotlib.pyplot as plt
 
-'''
-avg_ishf sensible heat flux
-avg_slhtf latent heat flux
-'''
+"""
+ERA5 variables
+avg_ishf   : sensible heat flux
+avg_slhtf  : latent heat flux
+avg_snswrf : net shortwave radiation
+avg_snlwrf : net longwave radiation
+"""
 
-def plot_monthly_line():
-    return
-
-if __name__ == '__main__':
-    # Define bounds 6.369454°S 106.655815°E
-    lon_min, lon_max, lat_min, lat_max = (106.67, 106.97, -6.09, -6.35)
-
-    # Open data
-    file_name = './data/lab/surface_budget/vars_ERA5_1991-2020_mean_rates.nc'
+def load_dataset(file_name):
     ds = xr.open_dataset(file_name)
 
-    # Choose variables
-    ds = ds[['avg_ishf', 'avg_slhtf', 'avg_snswrf', 'avg_snlwrf']]
+    variables = [
+        "avg_ishf",
+        "avg_slhtf",
+        "avg_snswrf",
+        "avg_snlwrf"
+    ]
 
-    # Slice to Jakarta
-    ds = ds.sel(
-        latitude=slice(lat_min, lat_max),
-        longitude=slice(lon_min, lon_max)
+    return ds[variables]
+
+
+def subset_region(ds, bounds):
+    return ds.sel(
+        latitude=slice(bounds["lat_min"], bounds["lat_max"]),
+        longitude=slice(bounds["lon_min"], bounds["lon_max"])
     )
 
-    # Compute monthly
+def compute_monthly(ds):
     ds_monthly = ds.groupby("valid_time.month").mean("valid_time")
-    print(ds_monthly)
 
-    # Initialize plot
+    # Sign convention (upward heat flux positive)
+    ds_monthly["avg_ishf"] = -ds_monthly["avg_ishf"]
+    ds_monthly["avg_slhtf"] = -ds_monthly["avg_slhtf"]
+    ds_monthly["avg_snlwrf"] = -ds_monthly["avg_snlwrf"]
+
+    # Net radiation
+    ds_monthly["Rs"] = ds_monthly["avg_snswrf"] - ds_monthly["avg_snlwrf"]
+
+    # Heat storage
+    ds_monthly["Storage"] = ds_monthly["Rs"] - (ds_monthly["avg_ishf"] + ds_monthly["avg_slhtf"])
+
+    return ds_monthly
+
+
+def plot_monthly_line(ds_monthly):
     fig, ax = plt.subplots(figsize=(12, 9))
 
-    # Flip sign
-    ds_monthly['avg_ishf'] *= -1.0
-    ds_monthly['avg_slhtf'] *= -1.0
+    ds_monthly["Rs"].plot(ax=ax, label="Net Radiation (Rs)", color='k')
+    ds_monthly['avg_snswrf'].plot(ax=ax, label="Net Shortwave Radiation", color='yellow', alpha=0.85)
+    ds_monthly['avg_snlwrf'].plot(ax=ax, label="Net Longwave Radiation", color='tab:green', alpha=0.85)
 
-    # Sensible heat cooling
-    ds_monthly['avg_ishf'].plot(ax=ax, label='SH')
-    ds_monthly['avg_slhtf'].plot(ax=ax, label='LH')
-    (ds_monthly['avg_snswrf'] + ds_monthly['avg_snlwrf']).plot(ax=ax, label='Rs')
-    plt.legend()
+    ds_monthly["avg_ishf"].plot(ax=ax, label="Sensible Heat (SH)", color='r', linestyle='--')
+    ds_monthly["avg_slhtf"].plot(ax=ax, label="Latent Heat (LH)", color='b', linestyle='--')
+    ds_monthly["Storage"].plot(ax=ax, label="Heat Storage", color='orange', linestyle='--')
+
+    # Activate texts
+    ax.set_ylabel("W m$^{-2}$")
+    ax.set_title("Monthly Mean Surface Heat Fluxes (ERA5, 1991-2020) - Jakarta")
+    ax.legend()
+
+    # Activate monthly index name
+    month_labels = [
+    "Jan","Feb","Mar","Apr","May","Jun",
+    "Jul","Aug","Sep","Oct","Nov","Dec"
+    ]
+    ax.set_xticks(range(1, 12+1))
+    ax.set_xticklabels(month_labels)
+
+    plt.grid()
     plt.show()
 
-    
-    
+
+if __name__ == "__main__":
+    # Data location
+    file_name = "./data/lab/surface_budget/vars_ERA5_1991-2020_mean_rates.nc"
+
+    # Jakarta bounds
+    bounds = dict(
+        lon_min=106.67,
+        lon_max=106.97,
+        lat_min=-6.09,
+        lat_max=-6.35
+    )
+    ds = load_dataset(file_name)
+    ds = subset_region(ds, bounds)
+    ds_monthly = compute_monthly(ds)
+    plot_monthly_line(ds_monthly)
