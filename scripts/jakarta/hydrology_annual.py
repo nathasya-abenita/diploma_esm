b3e596d935346760e3ed69df8b5099afd87cf3fc
Nathasya Christien <nathasya.abenita@gmail.com>
2026-03-22 02:06:33 +0100

    add more hydrological components

--- scripts/jakarta/hydrology_annual.py
+++ scripts/jakarta/hydrology_annual.py
@@ -17,26 +17,39 @@ if __name__ == '__main__':
     ds_pev = xr.open_dataset('./data/lab/surface_budget/pev_ymonmean.nc')
     ds_tp = xr.open_dataset('./data/lab/surface_budget/tp_ymonmean.nc')
     ds_t2m = xr.open_dataset('./data/lab/surface_budget/t2m_ymonmean.nc')
+    ds_ro = xr.open_dataset('./data/lab/surface_budget/ro_ymonmean.nc')
+    ds_m = xr.open_dataset('./data/lab/surface_budget/swvl1_ymonmean.nc')
+    ds_tcc = xr.open_dataset('./data/lab/surface_budget/tcc_ymonmean.nc')
 
     # Subset
     ds_e = representative_cell(ds_e, lon, lat)
     ds_pev = representative_cell(ds_pev, lon, lat)
     ds_tp = representative_cell(ds_tp, lon, lat)
     ds_t2m = representative_cell(ds_t2m, lon, lat)
+    ds_ro = representative_cell(ds_ro, lon, lat)
+    ds_m = representative_cell(ds_m, lon, lat)
+    ds_tcc = representative_cell(ds_tcc, lon, lat)
 
     # Convert unit
     ds_tp['tp'] = ds_tp['tp'] * 1e3 # m to mm
     ds_e['e'] = ds_e['e'] * -1e3 # m to mm, evapotranspiration to be positive
     ds_pev['pev'] = ds_pev['pev'] * -1e3 # m to mm
     ds_t2m['t2m'] = ds_t2m['t2m'] - 273.15 # k to deg c
+    ds_ro['ro'] *= 1e3
+
+    # Sum all ds_m
 
     # Initialize figure
-    fig, ax = plt.subplots(figsize=(12,6))
+    fig, (ax, ax_bottom) = plt.subplots(
+    2, 1, figsize=(12, 10), sharex=True,
+    gridspec_kw={'height_ratios': [2, 1]}
+    )
 
     # Plot data
     ax.plot(ds_tp['tp'], 'b--', label='P')
     ax.plot(ds_e['e'], 'r--', label='E')
     ax.plot(ds_pev['pev'], 'k', label='PET')
+    ax.plot(ds_ro['ro'], '--', color='gray', label='ΔF')
 
      # Create second y-axis for temperature
     ax2 = ax.twinx()
@@ -53,6 +66,16 @@ if __name__ == '__main__':
     ax.set_ylabel('mm/day')
     ax2.set_ylabel('Surface temperature (°C)', color='darkred')
     ax.legend(loc='upper right'); ax2.legend(loc='lower right')
+
+    # -------------------------
+    # BOTTOM PLOT
+    ax_bottom.plot(ds_m['swvl1'], color='purple', label='Surface moisture')
+    ax_bottom.plot(ds_tcc['tcc'], color='gray', label='Total cloud cover')
+
+    ax_bottom.set_ylabel('unitless')
+    ax_bottom.set_ylim(0, 1)
+    ax_bottom.set_xlabel('month')
+    ax_bottom.legend()
 
     #Title
     plt.title('Hydrological Cycle - Jakarta')
--- scripts/jakarta/hydrology_diurnal.py
+++ scripts/jakarta/hydrology_diurnal.py
@@ -3,25 +3,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 from module import representative_cell
 
-def plot_hourly_line(ds, ds_t2m, time_min, time_max, out_file_name, title):
-
+def prepare_ds (ds, time_min, time_max):
     ds_day = ds.assign_coords(valid_time = ds.valid_time + np.timedelta64(7, "h"))
     ds_day = ds_day.sel(valid_time=slice(time_min, time_max))
     ds_day = ds_day.groupby("valid_time.hour").mean()
+    return ds_day
 
-    ds_t2m = ds_t2m.assign_coords(valid_time = ds.valid_time + np.timedelta64(7, "h"))
-    ds_t2m = ds_t2m.sel(valid_time=slice(time_min, time_max))
-    ds_t2m = ds_t2m.groupby("valid_time.hour").mean()
+def plot_hourly_line(ds, ds_t2m, ds_swvl_tcc, ds_ro, time_min, time_max, out_file_name, title):
 
-    fig, ax = plt.subplots(figsize=(12, 6))
+    ds_day = prepare_ds(ds, time_min, time_max)
+    ds_t2m = prepare_ds(ds_t2m, time_min, time_max)
+    ds_ro = prepare_ds(ds_ro, time_min, time_max)
+    ds_swvl_tcc = prepare_ds(ds_swvl_tcc, time_min, time_max)
 
+    # Initialize figure
+    fig, (ax, ax_bottom) = plt.subplots(
+    2, 1, figsize=(12, 10), sharex=True,
+    gridspec_kw={'height_ratios': [2, 1]}
+    )
+
+    # Main hydrology cycle
     ax.plot(ds_day['tp'], 'b--', label='P')
     ax.plot(ds_day['e'], 'r--', label='E')
     ax.plot(ds_day['pev'], 'k', label='PET')
+    ax.plot(ds_ro['ro'], '--', color='gray', label='ΔF')
 
     # Create second y-axis for temperature
     ax2 = ax.twinx()
     ax2.plot(ds_t2m['t2m'], color='darkred', linewidth=2.5, marker='o', label='Temperature (°C)')
+
+    # BOTTOM PLOT
+    ax_bottom.plot(ds_swvl_tcc['swvl1'], color='purple', label='Surface moisture')
+    ax_bottom.plot(ds_swvl_tcc['tcc'], color='gray', label='Total cloud cover')
+    ax_bottom.set_ylabel('unitless')
+    ax_bottom.set_ylim(0, 1)
+    ax_bottom.set_xlabel('month')
+    ax_bottom.legend()
 
     ax.set_xlabel('Local Hour')
     ax.set_ylabel('mm/h')
@@ -43,6 +60,14 @@ if __name__ == "__main__":
     )
     lon, lat = (106.8229, -6.1944) # jakarta center
 
+    # Moisture and runoff for both data
+
+    ds_swvl_tcc = xr.open_dataset(r'data/lab/hourly_surface_budget/moisture_and_runoff/data_stream-oper_stepType-instant.nc')
+    ds_ro = xr.open_dataset(r'data/lab/hourly_surface_budget/moisture_and_runoff/data_stream-oper_stepType-accum.nc')
+    
+    ds_swvl_tcc = representative_cell(ds_swvl_tcc, lon, lat)
+    ds_ro = representative_cell(ds_ro, lon, lat)
+
     #%% Oct 2022
 
     file_name = "./data/lab/hourly_surface_budget/oct_2022/data_stream-oper_stepType-accum.nc"
@@ -57,8 +82,9 @@ if __name__ == "__main__":
     ds['e'] = ds['e'] * -1e3 # m to mm, evapotranspiration to be positive
     ds['pev'] = ds['pev'] * -1e3 # m to mm
     ds_t2m['t2m'] = ds_t2m['t2m'] - 273.15 # k to deg c
+    ds_ro['ro'] *= 1e3
 
-    plot_hourly_line(ds, ds_t2m, time_min='2022-10-01 00:00', time_max='2022-10-31 23:00',
+    plot_hourly_line(ds, ds_t2m, ds_swvl_tcc, ds_ro, time_min='2022-10-01 00:00', time_max='2022-10-31 23:00',
                      out_file_name='./output/hydrology/oct_diurnal_cycle.png',
                      title = 'Oct 2022')
     
@@ -77,6 +103,6 @@ if __name__ == "__main__":
     ds['pev'] = ds['pev'] * -1e3 # m to mm
     ds_t2m['t2m'] = ds_t2m['t2m'] - 273.15 # k to deg c
 
-    plot_hourly_line(ds, ds_t2m, time_min='2022-05-01 00:00', time_max='2022-05-31 23:00',
+    plot_hourly_line(ds, ds_t2m, ds_swvl_tcc, ds_ro, time_min='2022-05-01 00:00', time_max='2022-05-31 23:00',
                      out_file_name='./output/hydrology/may_diurnal_cycle.png',
                      title = 'May 2022')
